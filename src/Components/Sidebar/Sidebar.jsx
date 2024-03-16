f99931b65abd92e149457b86263bef69a91db3b3
TI Sabbir <tisabbir0@gmail.com>
2024-03-16 12:18:45 +0600

    want to cook can add data in side bar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,29 +1,37 @@
 const Sidebar = ({ recipez }) => {
   return (
-    <div className="border-2 border-[#28282833] border-solid rounded-2xl space-y-5">
+    <div className="border-2 border-[#28282833] border-solid rounded-2xl space-y-5 min-w-96">
       <h1 className="font-semibold text-2xl text-center mt-8">
         Want to Cook : 0{recipez.length}
       </h1>
       <hr />
 
       {/* table */}
-      <table className="para">
-        <tr>
+      <table className="para space-y-6 ">
+        <tr className="">
           <th>No</th>
           <th>Name</th>
           <th>Time</th>
           <th>Calories</th>
         </tr>
 
-        <tr>
-          <td>01</td>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-          <button className="btn btn-ghost bg-[#0BE58A] rounded-full">
-            Preparing
-          </button>
-        </tr>
+        {recipez.map((recipe, idx) => {
+          return (
+            <tr key={idx} className=" space-x-2  bg-[#28282808] mt-6">
+              <td>{idx}</td>
+              <td className=" max-w-28">{recipe.recipe_name}</td>
+              <td>
+                {recipe.preparing_time} <br /> mins
+              </td>
+              <td>
+                {recipe.calories} <br /> calories
+              </td>
+              <button className="btn bg-[#0BE58A] rounded-full text-black py-0">
+                Preparing
+              </button>
+            </tr>
+          );
+        })}
       </table>
       {/* table */}
     </div>
