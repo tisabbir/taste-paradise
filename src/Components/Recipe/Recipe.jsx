03940edb1b631c7c077819421d2be6f5adc12d2e
TI Sabbir <tisabbir0@gmail.com>
2024-03-16 11:17:36 +0600

    fetched recipes data from json and showed it in the ui, desing made as the figma

--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -1,9 +1,48 @@
-const Recipe = () => {
+import PropTypes from "prop-types";
+import { FaFireAlt, FaRegClock } from "react-icons/fa";
+const Recipe = ({ recipe }) => {
+  const {
+    recipe_id,
+    recipe_image,
+    recipe_name,
+    short_description,
+    ingredients,
+    preparing_time,
+    calories,
+  } = recipe;
+  console.log(recipe_image);
   return (
-    <div>
-      <h1>This is a recipe</h1>
+    <div className="p-6 border-2 border-solid border-[#28282833] rounded-2xl space-y-5">
+      <img className="rounded-2xl" src={recipe_image} alt="" />
+      <h2 className="text-xl font-semibold">{recipe_name}</h2>
+      <p className="para">{short_description}</p>
+      <hr />
+      <h2 className="text-xl font-medium text-[#282828] ">
+        Ingredients : {ingredients.length}
+      </h2>
+
+      <ul className="para">
+        {ingredients.map((item, idx) => {
+          return <li key={idx}>{item}</li>;
+        })}
+      </ul>
+
+      <hr />
+      <div className="flex items-center gap-3">
+        <FaRegClock></FaRegClock>
+        <p> {preparing_time} minutes</p>
+        <FaFireAlt></FaFireAlt>
+        <p> {calories} calories</p>
+      </div>
+      <button className="btn btn-ghost bg-[#0BE58A] rounded-full ">
+        Want to Cook
+      </button>
     </div>
   );
 };
 
+Recipe.propTypes = {
+  recipe: PropTypes.object.isRequired,
+  item: PropTypes.string,
+};
 export default Recipe;
--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+import Recipe from "../Recipe/Recipe";
 const Recipes = () => {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    fetch("recipe.json")
+      .then((res) => res.json())
+      .then((data) => setRecipes(data));
+  }, []);
+
   return (
-    <div className="border-2 border-black border-solid lg:w-2/3">
-      <h1>Thease are recipes</h1>
+    <div className=" lg:w-2/3">
+      <div className="recipe-container grid grid-cols-1 lg:grid-cols-2 gap-6">
+        {recipes.map((recipe, idx) => {
+          return <Recipe key={idx} recipe={recipe}></Recipe>;
+        })}
+      </div>
     </div>
   );
 };
